98c98556deea9d3ca2881544b6600cc7ef6c22f8
Tyler Niemann <tntman98@gmail.com>
2022-05-26 16:34:15 -0700

    create base files & base total calculator for shopping cart

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ShoppingCart from "./components/ShoppingCart";
+
+function App() {
+  return (
+    <div>
+      <ShoppingCart></ShoppingCart>
+    </div>
+  );
+}
+
+export default App;
---
+++ shopping-cart/src/components/ShoppingCart.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+function ShoppingCart(){
+    const [total,setTotal] = useState(0);
+    const [cart, setCart] = useState({
+       items : [
+        {
+        quantity : 2,
+        price : 50 
+        },
+        {
+         quantity : 1,
+         price : 50
+        }
+    ]
+});
+
+    const calculateTotal = (cart) => {
+      return cart.items.reduce((prev, curr) => prev + (curr.quantity * curr.price), 0)
+    }
+
+    const newTotal = () => {
+       setTotal(calculateTotal(cart))
+    }
+    
+    return (
+        <div>
+            <button onClick={newTotal}>Calc</button>
+            <h1>{total}</h1>
+        </div>
+    )
+}
+
+export default ShoppingCart
