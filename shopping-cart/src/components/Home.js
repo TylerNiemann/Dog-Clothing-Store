281ea2963a6e3d497d1a67a060aed7761f9dcb6e
Tyler Niemann <tntman98@gmail.com>
2023-06-20 15:05:05 -0700

    change long file name

--- shopping-cart/src/components/Home.js
+++ shopping-cart/src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
       <div id="home" >
        <h1>Mock E-Commerce Store</h1>
-       <img src={require('../components/images/Spring-Summer-Classic-Pet-Dog-Vest-T-Shirt-Striped-Dog-Shirt-XS-XXL-Pet-Clothes-For.jpg')} alt="product"></img>
+       <img src={require('./images/Dog.jpg')} alt="product"></img>
       </div>
     );
   };
